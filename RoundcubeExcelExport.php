2eabc7be3bb7f1f94ef73e026bdf5f01f9579c2b
Amel Junuzović <maniaba@outlook.com>
2024-08-17 13:40:28 +0200

    Setup composer and prepare class for load plugin

---
+++ RoundcubeExcelExport.php
@@ -0,0 +1,10 @@
+<?php
+
+use Hercegdoo\RoundcubeExcelExport\AbstractRoundcubeExcelExport;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, ['vendor', 'autoload.php']);
+
+final class RoundcubeExcelExport extends AbstractRoundcubeExcelExport
+{
+
+}
---
+++ src/AbstractRoundcubeExcelExport.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Hercegdoo\RoundcubeExcelExport;
+
+use rcube_plugin;
+
+abstract class AbstractRoundcubeExcelExport extends rcube_plugin
+{
+    public function init()
+    {
+        // TODO: Implement init() method.
+    }
+}
