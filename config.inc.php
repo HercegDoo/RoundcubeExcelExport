4ebcd9e64759aa44608d45ac7d91be89ae0c1289
Amel Junuzović <maniaba@outlook.com>
2024-08-18 14:25:13 +0200

    Plugin demo

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Roundcube excel export configuration file
+ */
+
+$config['memory_limit'] = '2048M';
+$config['max_execution_time'] = '900';
+$config['excel_charset'] = 'UTF-8';
+
+$config['limit_number_of_messages'] = 5000;
